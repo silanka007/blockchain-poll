19b3259a234116122fcb62a4ce4724dd6724ec5d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-03-15 20:07:31 +0100

    implementation of nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import { Button, Grid, withStyles } from '@material-ui/core'
+import React from 'react'
+
+const styles = {
+  navContainer: {
+    width: "100%",
+    height: 50,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "5px 20px"
+  }
+}
+
+export default withStyles(styles)(function Nav({classes}) {
+  return (
+    <Grid className={classes.navContainer}>
+      <a href="#!">Blockchain Poll</a>
+      <Button>Create Poll</Button>
+    </Grid>
+  )
+})
